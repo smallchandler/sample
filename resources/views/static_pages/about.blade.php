b16cd8c2e7cc35b47de9a3621ae7dc0512d3377e
Will <zhangwenhao@zerozero.cn>
2017-09-29 06:31:10 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('title', '关于')
+
+@section('content')
+    <h1>关于页</h1>
+@stop
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+    <title>Sample App</title>
+</head>
+<body>
+    <h1>关于页</h1>
+</body>
+</html> -->
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+@section('content')
+    <h1>主页</h1>
+@stop
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+    <title>Sample App</title>
+</head>
+<body>
+    <h1>主页</h1>
+</body>
+</html> -->
