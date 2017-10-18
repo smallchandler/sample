2547596566ece2753ff0b6a4a39f589636a810b2
Will <zhangwenhao@zerozero.cn>
2017-10-18 03:43:01 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'zhangwenhao@zerozero.cn';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
